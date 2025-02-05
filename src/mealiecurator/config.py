611cce496d0596cf085e26ee33365a723bf74b4b
Simone Gaiarin <simgunz@gmail.com>
2025-02-05 17:20:22 +0100

    feat: add default config directory in user home

--- src/mealiecurator/config.py
+++ src/mealiecurator/config.py
@@ -2,6 +2,10 @@
 
 from pathlib import Path
 
+from appdirs import user_config_dir
+
+CONFIG_DIR = Path(user_config_dir("mealiecurator", appauthor=False))
+
 
 def ensure_config_dir(config_dir_path: Path) -> None:
     """Ensure the config directory exists."""
