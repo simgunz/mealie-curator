ec598ee5d2173b4e2ab0cee85d1c4e15e1c1f183
Simone Gaiarin <simgunz@gmail.com>
2025-02-05 18:20:02 +0100

    feat: add config cli command to set and read config values

--- src/mealiecurator/cli.py
+++ src/mealiecurator/cli.py
@@ -1,9 +1,13 @@
 """mealiecurator CLI."""
+
+from pathlib import Path
 from typing import Annotated, Optional
 
+import appdirs
 import typer
 
 from mealiecurator import __version__, logs
+from mealiecurator.config import get_config
 from mealiecurator.logs import LogLevel
 
 app = typer.Typer()
@@ -38,3 +42,17 @@ def cli(
 ) -> None:
     """Engage with mealiecurator using this CLI."""
     logs.set_level(log_level.value)
+
+
+@app.command()
+def config(
+    key: Annotated[str, typer.Argument(help="Configuration key to get/set")],
+    value: Annotated[Optional[str], typer.Argument(help="Value to set")] = None,
+) -> None:
+    """Get or set a configuration value."""
+    config_dir_path = Path(appdirs.user_config_dir("mealiecurator", appauthor=False))
+    config_path = config_dir_path / "mealiecuratorrc"
+    config = get_config(config_path)
+    if value is None:
+        config_value = config[key]
+        typer.echo(config_value)
