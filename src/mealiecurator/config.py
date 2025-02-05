4c9d5cea7ccd646d6483f03e651b30459464371f
Simone Gaiarin <simgunz@gmail.com>
2025-02-05 17:40:31 +0100

    feat: get default config

--- src/mealiecurator/config.py
+++ src/mealiecurator/config.py
@@ -3,10 +3,24 @@
 from pathlib import Path
 
 from appdirs import user_config_dir
+from configobj import ConfigObj
+from validate import Validator
 
 CONFIG_DIR = Path(user_config_dir("mealiecurator", appauthor=False))
+CONFIG_SPEC = """
+[DEFAULT]
+""".splitlines()
 
 
 def ensure_config_dir(config_dir_path: Path) -> None:
     """Ensure the config directory exists."""
     config_dir_path.mkdir(parents=True, exist_ok=True)
+
+
+def get_config(config_path: Path) -> ConfigObj:
+    """Get the configuration object."""
+    ensure_config_dir(config_path.parent)
+    config = ConfigObj(str(config_path), configspec=CONFIG_SPEC)
+    validator = Validator()
+    config.validate(validator)
+    return config
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from mealiecurator.config import ensure_config_dir
+from mealiecurator.config import ensure_config_dir, get_config
 
 
 def test_ensure_config_dir_creates_new(tmp_path: Path):
@@ -20,3 +20,11 @@ def test_ensure_config_dir_existing(tmp_path: Path):
 
     assert config_path.exists()
     assert config_path.is_dir()
+
+
+def test_get_default_config(tmp_path: Path):
+    config_path = tmp_path / "config" / "configrc"
+
+    config = get_config(config_path)
+
+    assert config == {"DEFAULT": {}}
