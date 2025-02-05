20df3df2a4e225bd0eab38ece36e09b92501a0fa
Simone Gaiarin <simgunz@gmail.com>
2025-02-05 18:42:39 +0100

    feat: allow setting the configuration file

--- src/mealiecurator/cli.py
+++ src/mealiecurator/cli.py
@@ -10,6 +10,9 @@ from mealiecurator import __version__, logs
 from mealiecurator.config import get_config
 from mealiecurator.logs import LogLevel
 
+CONFIG_DIR_PATH = Path(appdirs.user_config_dir("mealiecurator", appauthor=False))
+DEFAULT_CONFIG_PATH = CONFIG_DIR_PATH / "mealiecuratorrc"
+
 app = typer.Typer()
 
 
@@ -48,10 +51,12 @@ def cli(
 def config(
     key: Annotated[str, typer.Argument(help="Configuration key to get/set")],
     value: Annotated[Optional[str], typer.Argument(help="Value to set")] = None,
+    config_path: Annotated[
+        Optional[Path],
+        typer.Option(help="Path to the configuration file"),
+    ] = DEFAULT_CONFIG_PATH,
 ) -> None:
     """Get or set a configuration value."""
-    config_dir_path = Path(appdirs.user_config_dir("mealiecurator", appauthor=False))
-    config_path = config_dir_path / "mealiecuratorrc"
     config = get_config(config_path)
     if value is None:
         config_value = config[key]
